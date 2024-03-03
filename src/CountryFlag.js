f6233b5f20cb8459ee87f899a90e7dc7f1d882c3
martinmazas <mazasmartin@gmail.com>
2024-03-03 12:20:36 +0200

    small fix

---
+++ src/CountryFlag.js
@@ -0,0 +1,21 @@
+import ReactCountryFlag from "react-country-flag"
+import { markCell } from "./Utils/functions"
+import React from "react"
+
+const styles = {
+    flag: {
+        fontSize: '15em'
+    }
+}
+
+const CountryFlag = (props) => {
+    const {country, cellIndex, table} = {...props}
+    
+    markCell(table, cellIndex, 0)
+
+    return (
+        <ReactCountryFlag svg countryCode={`${country.code}`} style={styles.flag} className={`flag-${country.name}`} />
+    )
+}
+
+export default CountryFlag
