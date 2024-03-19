ed4bca6218b802aebac2b8fbe09049ad56bc5702
martinmazas <mazasmartin@gmail.com>
2024-03-19 20:52:23 +0200

    addPlayer page added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddPlayer from './addPlayer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path='/' Component={App} />
+        <Route path='/addPlayer' Component={AddPlayer} />
+        {/* <App /> */}
+      </Routes>
+    </Router>
   </>
 );
