8f078df13d930bd03c60e2fba92ea1fc00413314
martinmazas <mazasmartin@gmail.com>
2024-05-30 18:51:20 +0300

    Update CircularIndeterminate.jsx

--- src/CircularIndeterminate.jsx
+++ src/CircularIndeterminate.jsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 
 export default function CircularIndeterminate() {
     return (
-        <Box sx={{ display: 'flex' }}>
+        <Box>
             <CircularProgress size={'10rem'} style={{ position: 'absolute', left: '50%', top: '50%' }} />
         </Box>
     );
