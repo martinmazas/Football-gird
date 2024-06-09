aaf7a136377b50183c5d3360b84a388e7b5e6c85
Martin Mazas <mazasmartin@gmail.com>
2024-06-09 07:38:43 +0300

    Update FullWidthTextField.jsx

--- src/FullWidthTextField.jsx
+++ src/FullWidthTextField.jsx
@@ -3,17 +3,33 @@ import Input from '@mui/material/Input';
 
 
 const styles = {
-  field: {
-    color: '#fff'
-  },
   box: {
     bgcolor: '#031825',
     maxWidth: '60rem',
     outline: 'none',
     padding: '0.2rem 0.4rem',
     borderRadius: '0.4rem',
-    border: '0.2rem solid #f2b705'
-  }
+    border: '0.2rem solid #f2b705',
+    '@media (minWidth: 600px)': {
+      padding: '0.4rem 0.8rem',
+      borderRadius: '0.6rem',
+    },
+    '@media (minWidth: 960px)': {
+      padding: '0.6rem 1.2rem',
+      borderRadius: '0.8rem',
+    },
+  },
+  field: {
+    color: '#fff',
+    width: '100%',
+    fontSize: '1rem',
+    '@media (minWidth: 600px)': {
+      fontSize: '1.2rem',
+    },
+    '@media (minWidth: 960px)': {
+      fontSize: '1.4rem',
+    },
+  },
 }
 
 export default function FullWidthTextField(props) {
