e6617b58903716203754cc9552d1084af23cc189
martinmazas <mazasmartin@gmail.com>
2024-10-13 10:39:37 +0300

    Update functions.jsx

--- src/Utils/functions.jsx
+++ src/Utils/functions.jsx
@@ -83,16 +83,16 @@ export const addPhoto = (players, setIsError, handleScore) => {
     }
 }
 
-export const LogService = (message, level = 'info') => {
-    const logEntry = {
-        message,
-        level,
-        timestamp: new Date().toISOString(),
-        location: window.location.href,
-    }
+export const LogService = async (message, level = 'info') => {
+    // const logEntry = {
+    //     message,
+    //     level,
+    //     timestamp: new Date().toISOString(),
+    //     location: window.location.href,
+    // }
 
     try {
-        axios.post(`${server}/api/logs`, { ...axiosConfig, logEntry });
+       await axios.post(`${server}/api/logs`, { ...axiosConfig, message });
     } catch (err) {
         handleError(err, 'Failed to fetch play parameters')
     }
