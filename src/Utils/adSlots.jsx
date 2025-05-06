229a8cd7ff5e0ba430a3141a8d8f7e152d5d87d2
martinmazas <mazasmartin@gmail.com>
2025-05-06 11:01:39 +0300

    Update adSlots.jsx

--- src/Utils/adSlots.jsx
+++ src/Utils/adSlots.jsx
@@ -1,11 +1,12 @@
 const tournamentAdSlots = {
     "Champions League 24/25": "9082587985",
-    "Europa League 24/25": "1947152235",
-    "Libertadores 25": "6815517295",
-    "MLS 25": "1563190611",
-    "Premier League": "8320988899",
-    "La Liga": "1775536505",
-    "Serie A": "6112939593"
+    "Europe League 24/25": "1947152235",
+    "Libertadores 2025": "6815517295",
+    "MLS 2025": "1563190611",
+    "Premier League 24/25": "8320988899",
+    "La Liga 24/25": "1775536505",
+    "Serie A 24/25": "6112939593",
+    "Bundesliga 24/25": "2187513741"
 };
 
 export default tournamentAdSlots;
