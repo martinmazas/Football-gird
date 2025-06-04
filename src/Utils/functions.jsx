0a021c0552d84980222a2b3359e4bdeef0cbb0a2
martinmazas <mazasmartin@gmail.com>
2025-06-04 23:41:41 +0300

    Update functions.jsx

--- src/Utils/functions.jsx
+++ src/Utils/functions.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import ResponsiveImage from '../ResponsiveImage';
 import { createRoot } from 'react-dom/client';
-const server = process.env.NODE_ENV === 'production' ? 'https://football-grid-edd30e867195.herokuapp.com' : 'http://localhost:8080';
+const server = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION_SERVER : 'http://localhost:8080';
+
 const axiosConfig = {
     headers: {
         "Content-Type": "application/json"
