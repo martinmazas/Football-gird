ded6a0e83fcd02ea6ef7002e5b631af5ef8d056d
martinmazas <mazasmartin@gmail.com>
2026-03-26 13:07:46 -0300

    name added to teams and countries

--- src/Components/CountryFlag.tsx
+++ src/Components/CountryFlag.tsx
@@ -3,6 +3,9 @@ import { CountryFlagProps } from "../Types/types";
 
 export default function CountryFlag({ country }: CountryFlagProps) {
     return (
-        <ReactCountryFlag svg countryCode={`${country.code}`} id={`flag-${country.name}`} />
+        <div className="grid-label-card">
+            <ReactCountryFlag svg countryCode={`${country.code}`} id={`flag-${country.name}`} />
+            <span className="grid-label-card__name">{country.name}</span>
+        </div>
     )
 }
--- src/Components/TeamFlag.tsx
+++ src/Components/TeamFlag.tsx
@@ -4,13 +4,16 @@ const s3Path = `${process.env.REACT_APP_S3}/images/Team-shield/`;
 
 const TeamFlag = ({team}: {team: Team}) => {
   return (
-    <a href={team.url} target="_blank" rel="noopener noreferrer">
-      <ResponsiveImage
-        id={`shield-${team.name}`}
-        src={`${s3Path}${team.code}.webp`}
-        alt={`${team.code}`}
-      />
-    </a>
+    <div className="grid-label-card">
+      <a href={team.url} target="_blank" rel="noopener noreferrer">
+        <ResponsiveImage
+          id={`shield-${team.name}`}
+          src={`${s3Path}${team.code}.webp`}
+          alt={`${team.code}`}
+        />
+      </a>
+      <span className="grid-label-card__name">{team.name}</span>
+    </div>
   );
 };
 
